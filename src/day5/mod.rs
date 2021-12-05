a432498a22a62555335cf95b36a196fa1a1a4789
Vitaly Domnikov <dotcypress@gmail.com>
2021-12-05 13:43:50 -0800

    day5 cleanup

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -9,29 +9,30 @@ struct VentsMap {
 
 impl VentsMap {
     fn parse(input: &str) -> Self {
-        Self {
-            vents: input.lines().map(Vent::parse).collect(),
-        }
+        let vents = input.lines().map(Vent::parse).collect();
+        Self { vents }
+    }
+
+    fn overlaps(&self, include_diagonals: bool) -> usize {
+        self.vents
+            .iter()
+            .filter(|vent| include_diagonals || !vent.is_diagonal())
+            .flat_map(|v| v.track())
+            .fold(HashMap::new(), |mut acc, point| {
+                *acc.entry(point).or_insert(0) += 1;
+                acc
+            })
+            .iter()
+            .filter(|x| *x.1 > 1)
+            .count()
     }
 
     fn part_one(self) -> usize {
-        let mut overlaps = HashMap::new();
-        for vent in self.vents.iter().filter(|vent| !vent.is_diagonal()) {
-            for point in vent.track() {
-                *overlaps.entry(point).or_insert(0) += 1;
-            }
-        }
-        overlaps.iter().filter(|x| *x.1 > 1).count()
+        self.overlaps(false)
     }
 
     fn part_two(self) -> usize {
-        let mut overlaps = HashMap::new();
-        for vent in self.vents.iter() {
-            for point in vent.track() {
-                *overlaps.entry(point).or_insert(0) += 1;
-            }
-        }
-        overlaps.iter().filter(|x| *x.1 > 1).count()
+        self.overlaps(true)
     }
 }
 
@@ -45,6 +46,11 @@ impl Point {
     pub fn new(x: isize, y: isize) -> Self {
         Self { x, y }
     }
+
+    pub fn parse(input: &str) -> Self {
+        let (x, y) = input.split_once(',').unwrap();
+        Self::new(x.parse().unwrap(), y.parse().unwrap())
+    }
 }
 
 struct Vent {
@@ -55,26 +61,10 @@ struct Vent {
 impl Vent {
     pub fn parse(input: &str) -> Self {
         let (p1, p2) = input.split_once(" -> ").unwrap();
-        let (x1, y1) = p1.split_once(',').unwrap();
-        let (x2, y2) = p2.split_once(',').unwrap();
         Self {
-            from: Point {
-                x: x1.parse().unwrap(),
-                y: y1.parse().unwrap(),
-            },
-            to: Point {
-                x: x2.parse().unwrap(),
-                y: y2.parse().unwrap(),
-            },
+            from: Point::parse(p1),
+            to: Point::parse(p2),
         }
-    }
-
-    pub fn is_vertical(&self) -> bool {
-        self.from.x == self.to.x
-    }
-
-    pub fn is_horizontal(&self) -> bool {
-        self.from.y == self.to.y
     }
 
     pub fn is_diagonal(&self) -> bool {
@@ -83,17 +73,15 @@ impl Vent {
 
     pub fn track(&self) -> Vec<Point> {
         let mut res = vec![];
-        if self.is_vertical() {
+        if self.from.x == self.to.x {
             for y in isize::min(self.from.y, self.to.y)..=isize::max(self.from.y, self.to.y) {
                 res.push(Point::new(self.from.x, y));
             }
-        }
-        if self.is_horizontal() {
+        } else if self.from.y == self.to.y {
             for x in isize::min(self.from.x, self.to.x)..=isize::max(self.from.x, self.to.x) {
                 res.push(Point::new(x, self.from.y));
             }
-        }
-        if self.is_diagonal() {
+        } else {
             let len = (self.to.x - self.from.x).abs();
             let step_x = (self.to.x - self.from.x).signum();
             let step_y = (self.to.y - self.from.y).signum();
